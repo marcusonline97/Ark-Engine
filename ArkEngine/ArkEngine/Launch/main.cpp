aaa8d8a05e5849be8e09feffbf621be3381ba818
marcusonline97 <Mjpersson97@gmail.com>
2025-12-08 21:27:57 +0100

    Window Creation Successful
    ToDo: Create a singleton of the instance and also add my own custom logger

--- ArkEngine/ArkEngine/Launch/main.cpp
+++ ArkEngine/ArkEngine/Launch/main.cpp
@@ -1,92 +1,32 @@
-#include "StartingApp.hpp"
-
-//std
-#include <cstdlib>
-#include <iostream>
-#include <stdexcept>
-int main()
-{
-	vk::StartApp app{};
-
-	try
-	{
-		app.run();
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS;
-}
-
-
-
-/*
-#include <stdio.h>
-#include <stdlib.h>
-
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
+#define GLM_FORCE_RADIANS //vulkan specific
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
+#include <glm/glm.hpp> //It seems like my vulkan SDK actually come prepacked with glm for some reason
+//And my setup seems to wanna link to both my Vulkan SDK and my local GLM inside of the project so be mindful of the pathing
+
+#include <glm/mat4x4.hpp> 
+
 #include <iostream>
 
-#define WINDOW_WIDTH 1920
-#define WINDOW_HEIGHT 1080
-
-GLFWwindow* window = NULL;
-
-void GLFW_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+int main()
 {
-	if ((key == GLFW_KEY_ESCAPE) && (action == GLFW_PRESS))
-	{
-		glfwSetWindowShouldClose(window, GLFW_TRUE);
-	}
-}
+	glfwInit();
+	
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 670, "Ark Engine", nullptr, nullptr);
 
-void CheckInstanceExtensionProperties()
-{
 	uint32_t extensionCount = 0;
 	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-	std::cout << extensionCount << " extension supported\n";
+	printf("Extension Count: %u\n", extensionCount); // switch out with custom logger later
 
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
-}
+	glm::mat4 testMatrix(1.0f);
+	glm::vec4 testVector(1.0f);
 
-int main(int argc, char* argv[])
-{
-	if (!glfwInit())
-	{
-		return 1;
-	}
+	auto testResult = testMatrix * testVector;
 
-	if (!glfwVulkanSupported())
-	{
-		return 1;
-	}
-
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Ark Engine version 0.1", NULL, NULL);
-
-	if (!window)
-	{
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
-
-	CheckInstanceExtensionProperties();
-
-	glfwSetKeyCallback(window, GLFW_KeyCallback);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -94,7 +34,9 @@ int main(int argc, char* argv[])
 	}
 
 	glfwDestroyWindow(window);
-
+	
 	glfwTerminate();
+
+	return 0;
 }
-*/
+
