82d5fc902996a437f0a8cf1d92b7aa2e2b84361a
marcusonline97 <Mjpersson97@gmail.com>
2026-06-15 23:59:56 +0200

    Added some models
    - cleaned out the old resource folder from garbage files.

--- ArkEngine/ArkEngine/Resources/Shaders/Fragment.glsl
+++ ArkEngine/ArkEngine/Resources/Shaders/Fragment.glsl
@@ -1,20 +1,14 @@
 #version 330 core
 
-#define MAX_LIGHTS 8
-
 struct Light
 {
-    vec3  color;
-    vec3  position;
-    vec3  direction;
-    float intensity;
-    float range;
-    int   type;       // 0 = directional, 1 = point
+    vec3 color;
+    vec3 direction;
 };
 
-uniform Light  uLights[MAX_LIGHTS];
-uniform int    uLightCount;
-uniform vec3   uCameraPos;
+uniform Light uLight;
+uniform vec3 uCameraPos;
+uniform vec3 color;
 
 out vec4 FragColor;
 
@@ -23,56 +17,29 @@ in vec3 vNormal;
 in vec3 vFragPos;
 
 uniform sampler2D baseColorTexture;
-uniform sampler2D specularMap;
-uniform int       uHasSpecularMap;
-uniform float     uSpecularStrength;
 
 void main()
 {
-    vec3 norm    = normalize(vNormal);
+    vec3 norm = normalize(vNormal);
+    
+    // diffuse
+    vec3 lightDir = normalize(-uLight.direction);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * uLight.color;
+
+    // specular
     vec3 viewDir = normalize(uCameraPos - vFragPos);
+    vec3 redlectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, redlectDir), 0.0), 32.0);
+    float specularStrength = 0.5;
+    vec3 specular = specularStrength * spec * uLight.color;
 
+    // ambient
+    const float ambientStrength = 0.4;
+    vec3 ambient = ambientStrength * uLight.color;
+    
     vec4 texColor = texture(baseColorTexture, vUV);
+    vec3 result = (diffuse + specular + ambient) * texColor.xyz * color;
 
-    const float ambientStrength = 0.15;
-    vec3 result = vec3(0.0);
-
-    for (int i = 0; i < uLightCount; ++i)
-    {
-        vec3  lightDir;
-        float attenuation = 1.0;
-        vec3  radiance    = uLights[i].color * uLights[i].intensity;
-
-        if (uLights[i].type == 1)
-        {
-            // Point light
-            vec3  toLight = uLights[i].position - vFragPos;
-            float dist    = length(toLight);
-            if (dist > uLights[i].range) continue;
-            lightDir    = normalize(toLight);
-            attenuation = 1.0 / (1.0 + 0.09 * dist + 0.032 * dist * dist);
-        }
-        else
-        {
-            // Directional light
-            lightDir = normalize(-uLights[i].direction);
-        }
-
-        float diff    = max(dot(norm, lightDir), 0.0);
-        vec3  diffuse = diff * radiance;
-
-        vec3  reflectDir = reflect(-lightDir, norm);
-        float spec       = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
-        float specStr    = uSpecularStrength;
-        if (uHasSpecularMap != 0)
-            specStr *= texture(specularMap, vUV).r;
-        vec3 specular = specStr * spec * radiance;
-
-        result += (diffuse + specular) * attenuation;
-    }
-
-    vec3 ambientColor = (uLightCount > 0) ? uLights[0].color : vec3(1.0);
-    result += ambientStrength * ambientColor;
-
-    FragColor = texColor * vec4(result, 1.0);
+    FragColor = vec4(result, 1.0);
 }
